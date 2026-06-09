88287a454dc19a134069aa728ffc17d3cb21ff79
Your Name <you@example.com>
2026-06-09 20:26:13 +0330

    feat(4th): implementd problem 4

---
+++ 4-Monitoring/scripts/restart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+docker compose -f ../2-SafeRedis/compose.yaml down
+docker compose -f ../3-RedisExporter/compose.yaml down
+docker compose down
+docker compose up -d --wait
+
+./scripts/test.sh
---
+++ 4-Monitoring/scripts/test.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo 'To be implemented...'
