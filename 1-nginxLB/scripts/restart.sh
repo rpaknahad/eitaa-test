f61593302c3492fc516f3a35c06c171a78b08f48
Your Name <you@example.com>
2026-06-09 12:53:15 +0330

    chore(1st): corrected path refrencing at restart script

--- 1-nginxLB/scripts/restart.sh
+++ 1-nginxLB/scripts/restart.sh
@@ -2,20 +2,18 @@
 
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-${SCRIPT_DIR}/clear-containers.sh
+scripts/clear-containers.sh
 
 method=${1:-compose}
 
 if [[ "$method" == "compose" ]]; then
     echo "Trying compose method..."
-    docker compose -f ${SCRIPT_DIR}/../compose.yaml up -d --wait
+    docker compose -f compose.yaml up -d --wait
 elif [[ "$method" == "run" ]]; then
     echo "Trying docker run method..."
-    ${SCRIPT_DIR}/docker-run.sh
+    scripts/docker-run.sh
 else
     echo "Unknown method $method" && exit 1;
 fi
 
-${SCRIPT_DIR}/test.sh
+scripts/test.sh
